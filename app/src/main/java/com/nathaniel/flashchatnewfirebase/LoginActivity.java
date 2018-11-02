15a4c50b051b6c3a8df1b9e1bc6de5a5a0b72fac
Nat-A <iam.nathaniel@outlook.com>
2018-11-02 15:04:19 +0000

    this is a stable version of the app. sign in and sign up auth is working just fine

--- app/src/main/java/com/nathaniel/flashchatnewfirebase/LoginActivity.java
+++ app/src/main/java/com/nathaniel/flashchatnewfirebase/LoginActivity.java
@@ -1,9 +1,10 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.nathaniel.flashchatnewfirebase;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -14,12 +15,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     // TODO: Add member variables here:
+    private FirebaseAuth mAuth;
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -44,18 +49,21 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // TODO: Grab an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
     // Executed when Sign in button pressed
     public void signInExistingUser(View v)   {
         // TODO: Call attemptLogin() here
+        attemptLogin();
 
     }
 
     // Executed when Register button pressed
     public void registerNewUser(View v) {
-        Intent intent = new Intent(this, com.londonappbrewery.flashchatnewfirebase.RegisterActivity.class);
+        Intent intent = new Intent(this, com.nathaniel.flashchatnewfirebase.RegisterActivity.class);
         finish();
         startActivity(intent);
     }
@@ -63,15 +71,44 @@ public class LoginActivity extends AppCompatActivity {
     // TODO: Complete the attemptLogin() method
     private void attemptLogin() {
 
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if(email.equals("") || password.equals("")) return;
+        Toast.makeText(this, "Login in progreess...", Toast.LENGTH_SHORT).show();
 
         // TODO: Use FirebaseAuth to sign in with email & password
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
+                Log.d("FlashChat", "signInWithEmail() onComplete: " + task.isSuccessful());
+
+                if(!task.isSuccessful()){
+
+                    Log.d("FlashChat", "Problem signin in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
 
 
     }
 
     // TODO: Show error on screen with an alert dialog
-
+    private void showErrorDialog(String message) {
+        
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 
 }
--- app/src/test/java/com/nathaniel/flashchatnewfirebase/ExampleUnitTest.java
+++ app/src/test/java/com/nathaniel/flashchatnewfirebase/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.nathaniel.flashchatnewfirebase;
 
 import org.junit.Test;
 
